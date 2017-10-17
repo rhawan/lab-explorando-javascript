9e027c3aeb1a7c76556480c2401bf88d4300e3c3
Rhawan Brenner <rb.brenner10@gmail.com>
2017-10-17 14:54:46 -0200

    Workshop Explorando a Linguagem JavaScript

--- debug-com-developer-tools/total.js
+++ debug-com-developer-tools/total.js
@@ -8,10 +8,14 @@ console.log('totalElement', totalElement);
 
 valorElement.addEventListener('keyup', function(e) {
 	var total = 0;
-	var valor = parseFloat(valorElement.value);
+	var valor = parseFloat(valorElement.value ? valorElement.value : 0);
 
-	if (valor < 10) {
+	if (valor > 0 && valor < 10) {
 		total = valor + valor * 0.3 + 5;
+	} else if (valor >= 10 && valor < 50 ) {
+		total = valor + valor * 0.1 + 2;
+	} else {
+		total = valor;
 	}
 
 	totalElement.innerHTML = total;
