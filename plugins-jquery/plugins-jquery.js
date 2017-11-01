923fba4b6c4b2cd7e679f00c891b5e5656f077a1
Rhawan Brenner <rb.brenner10@gmail.com>
2017-11-01 15:08:53 -0200

    Workshop Explorando a Linguagem JavaScript

---
+++ plugins-jquery/plugins-jquery.js
@@ -0,0 +1,37 @@
+console.log('Iniciando...');
+
+$(function () {
+	var valorElement = $('#valor');
+	var totalElement = document.getElementById('total');
+	var totalMascara = $('#total-mascara');
+	var mascaraConfig = {
+		decimal: ',',
+		thousands: '.',
+		prefix: 'R$ '
+	};
+
+	console.log('valorElement', valorElement);
+	console.log('totalElement', totalElement);
+
+	valorElement.on('keyup', function(e) {
+	var total = 0;
+	var valor = valorElement.maskMoney('unmasked')[0];
+	//var valor = parseFloat(valorElement.val() ? valorElement.val() : 0);
+
+	if (valor > 0 && valor < 10) {
+		total = valor + valor * 0.3 + 5;
+	} else if (valor >= 10 && valor < 50 ) {
+		total = valor + valor * 0.1 + 2;
+	} else {
+		total = valor;
+	}
+
+	totalMascara.maskMoney(mascaraConfig);
+	totalMascara.maskMoney('mask', total);
+
+	totalElement.innerHTML = totalMascara.val();
+	console.log('valor', valorElement.val());
+	});
+
+	valorElement.maskMoney(mascaraConfig);
+});
