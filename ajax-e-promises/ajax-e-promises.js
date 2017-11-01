970f96b142ec4571ef15a92849cda67cb746e143
Rhawan Brenner <rb.brenner10@gmail.com>
2017-11-01 10:46:03 -0200

    Workshop Explorando a Linguagem JavaScript

--- ajax-e-promises/ajax-e-promises.js
+++ ajax-e-promises/ajax-e-promises.js
@@ -7,7 +7,7 @@ $(function() {
 
 	resposta.done(function(estados) {
 		var comboEstado = $('#combo-estado');
-		console.log('Voltou com sucesso!');
+		console.log('Estados:', estados);
 		estados.forEach(function(estado) {
 			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
 			comboEstado.append(optionEstado);
@@ -18,4 +18,38 @@ $(function() {
 		alert('Erro ao pesquisar estados no servidor!!!');
 	});
 
+});
+
+$(function() {
+
+	$('#combo-estado').change(function() {
+		$('#combo-cidade').prop('disabled', false);
+
+		var estadoSelecionado = $('#combo-estado').val();
+		var resposta = $.ajax({
+			url: 'http://localhost:8888/cidades?uf=' + estadoSelecionado,
+			method: 'GET',
+			dataType: 'jsonp'
+		});
+
+		resposta.done(function(cidades) {
+			var comboCidade = $('#combo-cidade');
+			comboCidade.empty();
+			console.log('Cidades:', cidades);
+			cidades.forEach(function(cidade) {
+				var optionCidade = $('<option>').val(cidade.codigo).text(cidade.nome);
+				comboCidade.append(optionCidade);
+			});
+		});
+
+		resposta.fail(function() {
+			alert('Erro ao pesquisar cidades no servidor!!!');
+		});
+
+	});
+
+	$('#combo-cidade').change(function() {
+		alert('Cidade selecionada: ' + $('#combo-cidade').val());
+	});
+	
 });
